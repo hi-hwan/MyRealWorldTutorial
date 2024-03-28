e66f2fe667df4f1fe4e25edaceb4041beaad7942
Inhwan Hwang <inhwan89@gmail.com>
2024-03-28 11:28:41 +0900

    AuthenticationInterceptorTest 의 expriedToken 테스트 추가

--- app/src/test/java/com/realworld/android/petsave/common/data/api/interceptors/AuthenticationInterceptorTest.kt
+++ app/src/test/java/com/realworld/android/petsave/common/data/api/interceptors/AuthenticationInterceptorTest.kt
@@ -3,6 +3,7 @@ package com.realworld.android.petsave.common.data.api.interceptors
 import com.google.common.truth.Truth.assertThat
 import com.realworld.android.petsave.common.data.api.ApiConstants
 import com.realworld.android.petsave.common.data.api.ApiParameters
+import com.realworld.android.petsave.common.data.api.utils.JsonReader
 import com.realworld.android.petsave.common.data.preferences.Preferences
 import okhttp3.OkHttpClient
 import okhttp3.Request
@@ -19,7 +20,7 @@ import org.mockito.Mockito.inOrder
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.times
 import org.mockito.Mockito.verify
-import org.mockito.Mockito.verifyNoInteractions
+import org.mockito.Mockito.verifyNoMoreInteractions
 import org.mockito.Mockito.`when`
 import org.robolectric.RobolectricTestRunner
 import java.time.Instant
@@ -46,13 +47,15 @@ class AuthenticationInterceptorTest {
         mockWebServer.start(8080)
 
         authenticationInterceptor = AuthenticationInterceptor(preferences)
-        okHttpClient = OkHttpClient().newBuilder().addInterceptor(authenticationInterceptor).build()
+        okHttpClient = OkHttpClient()
+            .newBuilder()
+            .addInterceptor(authenticationInterceptor)
+            .build()
     }
 
     @Test
     fun authenticationInterceptor_validToken() {
         // Given
-        // `when`은 이 테스트에 대해 모의로 반환해야 할 내용을 설정
         `when`(preferences.getToken()).thenReturn(validToken)
         `when`(preferences.getTokenExpirationTime()).thenReturn(
             Instant.now().plusSeconds(3600).epochSecond
@@ -61,7 +64,38 @@ class AuthenticationInterceptorTest {
 
         // When
         okHttpClient.newCall(
-            Request.Builder().url(mockWebServer.url(ApiConstants.ANIMALS_ENDPOINT)).build()
+            Request.Builder()
+                .url(mockWebServer.url(ApiConstants.ANIMALS_ENDPOINT))
+                .build()
+        ).execute()
+
+        // Then
+        val request = mockWebServer.takeRequest() // HTTP 요청을 기다린다.
+
+        // request 를 범위로 설정하고 몇가지 요청 매개변수를 확인
+        with (request) {
+            assertThat(method).isEqualTo("GET")
+            assertThat(path).isEqualTo(animalsEndpointPath)
+            assertThat(getHeader(ApiParameters.AUTH_HEADER))
+                .isEqualTo(ApiParameters.TOKEN_TYPE + validToken)
+        }
+    }
+
+    @Test
+    fun authenticationInterceptor_expiredToken() {
+        // Given
+        // `when`은 이 테스트에 대해 모의로 반환해야 할 내용을 설정
+        `when`(preferences.getToken()).thenReturn(expiredToken)
+        `when`(preferences.getTokenExpirationTime()).thenReturn(
+            Instant.now().minusSeconds(3600).epochSecond
+        )
+        mockWebServer.dispatcher = getDispatcherForExpiredToken()
+
+        // When
+        okHttpClient.newCall(
+            Request.Builder()
+                .url(mockWebServer.url(ApiConstants.ANIMALS_ENDPOINT))
+                .build()
         ).execute()
 
         // Then
@@ -85,19 +119,34 @@ class AuthenticationInterceptorTest {
         verify(preferences, times(1)).getTokenExpirationTime()
         verify(preferences, times(1)).putTokenExpirationTime(anyLong())
         verify(preferences, times(1)).putTokenType(ApiParameters.TOKEN_TYPE.trim())
-        verifyNoInteractions(preferences) // 이외의 다른 메서드가 호출되지 않는지 확인
+        verifyNoMoreInteractions(preferences) // 이외의 다른 메서드가 호출되지 않는지 확인
 
         // 다른 테스트와 마찬가지로 동물요청을 확인
         with(animalsRequest) {
             assertThat(method).isEqualTo("GET")
             assertThat(path).isEqualTo(animalsEndpointPath)
-            assertThat(getHeader(ApiParameters.AUTH_HEADER)).isEqualTo(ApiParameters.TOKEN_TYPE + validToken)
+            assertThat(getHeader(ApiParameters.AUTH_HEADER))
+                .isEqualTo(ApiParameters.TOKEN_TYPE + validToken)
         }
     }
 
     private fun getDispatcherForValidToken() = object : Dispatcher() {
         override fun dispatch(request: RecordedRequest): MockResponse {
             return when (request.path) {
+                animalsEndpointPath -> MockResponse().setResponseCode(200)
+                else -> MockResponse().setResponseCode(404)
+            }
+        }
+    }
+
+    private fun getDispatcherForExpiredToken() = object : Dispatcher() {
+        override fun dispatch(request: RecordedRequest): MockResponse {
+            return when (request.path) {
+                authEndpointPath -> {
+                    MockResponse().setResponseCode(200)
+                        .setBody(JsonReader.getJson("networkresponses/validToken.json"))
+                }
+
                 animalsEndpointPath -> MockResponse().setResponseCode(200)
                 else -> MockResponse().setResponseCode(404)
             }

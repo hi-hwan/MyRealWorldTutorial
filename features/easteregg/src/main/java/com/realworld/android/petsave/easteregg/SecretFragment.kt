7396bf3338fe8cf8673e55cbd450c114f32bd233
Inhwan Hwang <inhwan89@gmail.com>
2024-10-18 01:21:31 +0900

    Easter egg 모듈 추가

---
+++ features/easteregg/src/main/java/com/realworld/android/petsave/easteregg/SecretFragment.kt
@@ -0,0 +1,23 @@
+package com.realworld.android.petsave.easteregg
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.realworld.android.petsave.onboarding.databinding.FragmentSecretBinding
+
+class SecretFragment : Fragment() {
+    private val binding get() = _binding!!
+    private var _binding: FragmentSecretBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSecretBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+}
